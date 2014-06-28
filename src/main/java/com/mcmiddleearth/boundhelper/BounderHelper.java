4a0ee014c390c586bbbfd8a97aac00f12f21e170
Donovan S. Allen <dono.allen@gmail.com>
2014-06-28 12:17:05 -0600

    Plugin setup

---
+++ src/main/java/com/mcmiddleearth/boundhelper/BounderHelper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mcmiddleearth.boundhelper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Donovan
+ */
+public class BounderHelper extends JavaPlugin{
+    @Override
+    public void onEnable(){
+        
+    }
+    @Override
+    public void onDisable(){
+        
+    }
+    
+}
