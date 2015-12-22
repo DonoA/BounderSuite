24850d7c7f0ea6ab10e54815b01a0a2b4c71123f
Donovan Allen <dallen@dallen.xyz>
2015-12-21 21:22:20 -0700

    forgot webcode.. again

--- WebCode/library/Dev/PageCallback/OBForm.php
+++ WebCode/library/Dev/PageCallback/OBForm.php
@@ -1,31 +1,39 @@
 <?php
 class Dev_PageCallback_OBForm{
-    
+
     public static function respond(XenForo_Controller $controller, XenForo_ControllerResponse_Abstract $response){
-        $host = "tcp://localhost"; 
+        $key = substr(md5(rand()), 0, 7);
+        $host = "tcp://localhost";
         $port = 6789;
-        $data = 'ping/-/' . PHP_EOL;  //Adding PHP_EOL was the other part of the solution
+        $ping = 'ping/-/' . PHP_EOL;  //Adding PHP_EOL was the other part of the solution
+        $vald = 'validate/-/12341234/-/' . $key . PHP_EOL;
         $errstr = '';
         $errno = '';
-		$fp = @fsockopen($host, $port, $errno, $errstr, 1);
+	      $fp = @fsockopen($host, $port, $errno, $errstr, 1);
         if (!$fp){
-			$response->params['up'] = 0;
-            $obs = array();
+			       $response->params['up'] = 0;
+             $obs = array();
         }else {
-			$response->params['up'] = 1;
-            fwrite($fp, $data);
-            while (! feof($fp)) {
-                $obs = json_decode(fgets($fp, 4096));
-            }
-            fclose($fp);
+			       $response->params['up'] = 1;
+             fwrite($fp, $ping);
+             while (! feof($fp)) {
+                  $obs = json_decode(fgets($fp, 4096));
+             }
+             fclose($fp);
+        }
+        $fp = @fsockopen($host, $port, $errno, $errstr, 1);
+        if (!$fp){
+			       $response->params['up'] = 0;
+        }else {
+             fwrite($fp, $vald);
+             while (! feof($fp)) {
+                  $pass = fgets($fp, 4096);
+             }
+             fclose($fp);
         }
         $response->params['obs'] = $obs;
+        $response->params['key'] = $key;
         $response->templateName = 'OBFormTemplate';
     }
 }
 ?>
-
-
-
-
-
